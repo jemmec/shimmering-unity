fda8eeae20d9ee6e9d416db1c97a8adf0513f5d3
jemmec <jeremy.mcdade@unisa.edu.au>
2023-07-18 18:24:57 +0930

    Added shimmer library rev5 dll for HR algos

--- Assets/Addons/ShimmeringUnity/Scripts/Examples/ShimmerHeartrateMonitor.cs
+++ Assets/Addons/ShimmeringUnity/Scripts/Examples/ShimmerHeartrateMonitor.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ShimmerAPI;
+using ShimmerLibrary;
 
-public class ShimmerHeartrateMonitor : MonoBehaviour
+namespace ShimmeringUnity
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    /// <summary>
+    /// Basic example of measuring heart rate from the shimmer device
+    /// Ensure the ShimmerDevice has internalExpPower enabled and the
+    /// INTERNAL_ADC_A13 sensor enabled.
+    /// </summary>
+    public class ShimmerHeartrateMonitor : MonoBehaviour
     {
-        
+
+        PPGToHRAlgorithm PPGtoHeartRateCalculation;
+
+        private void Start()
+        {
+
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
